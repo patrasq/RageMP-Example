756b6d9ff8fa3f5f37c83d99e152506c8a3ec0e3
Laur <zii_mersi@yahoo.com>
2017-12-31 17:26:51 +0200

    Update db.js
    For those who don't know about mysqljs from node js.

--- packages/keker/modules/db.js
+++ packages/keker/modules/db.js
@@ -4,14 +4,14 @@ module.exports = {
 	Handle: null,
     Connect: function(callback) {
         this.Handle = mysql.createConnection({
-            host     : 'localhost',
+            host     : 'localhost', //do not use ip address as a host, create a dns if your mysql server is not hosted on same server/machine as ragemp server
             user     : 'root',
             password : '',
             database : 'gta5'
         });
         this.Handle.connect(function(e) {
             if ( !e ) { callback(); }
-            else console.log ("<b style='color:red;'>DATABASE ERROR</b>" + e);
+            else console.log ("DATABASE ERROR" + e);
          });        
     }
 };
